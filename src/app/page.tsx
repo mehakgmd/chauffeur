865eada6544862603aa6bfefbac02817b0b69c61
mehakgmd <mehakgmd01@gmail.com>
2026-02-05 18:54:57 +0500

    errors resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 'use client';
-import Image from "next/image";
-import styles from "./page.module.css";
-import LuxuryCarHero from "@/components/Hero";
 import Hero from "@/components/Hero";
 
 export default function Home() {
